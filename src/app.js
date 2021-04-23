19e51342422d17beaaaa6244c5f3d128e5703f3e
Kevin Sarges <kevinsarges08@gmail.com>
2021-04-22 21:47:35 -0300

    modificando o app.js

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const municipios = require('./routes/municios');
+const routesMunicipios = require('./routes/routesMunicipios');
+const routesPraias = require('./routes/routesPraias')
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(municipios);
+
+app.use(routesMunicipios);
+app.use(routesPraias);
 
 app.listen(port, () => {
 	console.log(`http://localhost:${port}`);
