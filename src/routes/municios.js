c551e60ed0e5478a10cf9957e3e24baa27216021
Kevin Sarges <kevinsarges08@gmail.com>
2021-04-22 21:44:55 -0300

    Add rotas

--- src/routes/municios.js
+++
@@ -1,8 +0,0 @@
-const routes = require('express').Router();
-const municipios = require('../controllers/municipos');
-
-routes.get('/municipios', municipios.list);
-
-routes.post('/post', municipios.post);
-
-module.exports = routes;
