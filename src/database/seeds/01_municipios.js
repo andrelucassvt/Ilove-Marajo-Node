6bbc6841cf5cc9c6dfcb8af7226521a5a067b256
Kevin Sarges <kevinsarges08@gmail.com>
2021-04-30 17:30:39 -0300

    Recriando seed

--- src/database/seeds/01_municipios.js
+++ src/database/seeds/01_municipios.js
@@ -5,54 +5,7 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('municipios').insert([
-        {
-          nome_municipios: "Ponta de Pedras"
-        },
-
-        {
-          nome_municipios: "Soure"
-        },
-
-        {
-          nome_municipios: "Afuá"
-        },
-
-        {
-          nome_municipios: "Anajás"
-        },
-
-        {
-          nome_municipios: "Breves"
-        },
-
-        {
-          nome_municipios: "Cachoeira do Arari"
-        },
-
-        {
-          nome_municipios: "Chaves"
-        },
-
-        {
-          nome_municipios: "Curralinho"
-        },
-
-        {
-          nome_municipios: "Muaná"
-        },
-
-        {
-            nome_municipios: "Salvaterra"
-        },
-
-        {
-          nome_municipios: "Santa Cruz do Arari"
-        },
-        
-        {
-            nome_municipios: "São Sebastião da Boa Vista"
-        }
-
+        { nome_municipios: "Salvaterra" },
       ]);
     });
 };
