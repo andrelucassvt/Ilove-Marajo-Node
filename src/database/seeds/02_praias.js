c5ccf27fab47ac34763d5d82d376951db85b29ee
Kevin Sarges <kevinsarges08@gmail.com>
2021-05-02 09:36:42 -0300

    Alterando o tipo de dado

--- src/database/seeds/02_praias.js
+++ src/database/seeds/02_praias.js
@@ -6,15 +6,17 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('praia').insert([
         {
+          id_praia: 1,
           nome_praia: 'Praia Grande',
           foto: 'https://www.google.com/maps/place/Praia+Grande+de+Salvaterra/@-0.7628496,-48.511852,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipMg4Q4SrXgvtSQWROr2c14oiOGNQ2eJo4qSsn-s!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipMg4Q4SrXgvtSQWROr2c14oiOGNQ2eJo4qSsn-s%3Dw360-h270-k-no!7i2048!8i1536!4m5!3m4!1s0x92a6bba82d1009c9:0xf8b205fbe791e347!8m2!3d-0.7628496!4d-48.511852',
           lat: '-0.7628496',
           long: '-48.511852',
-          avaliacao: 4.5,
+          avaliacao: 4.50,
           municipios: 'Salvaterra',
         },
 
         {
+          id_praia: 2,
           nome_praia: 'Praia Portinho',
           foto: 'https://www.google.com/maps/place/Praia+Grande+de+Salvaterra/@-0.7628496,-48.511852,3a,75y,90t/data=!3m8!1e2!3m6!1sAF1QipMg4Q4SrXgvtSQWROr2c14oiOGNQ2eJo4qSsn-s!2e10!3e12!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipMg4Q4SrXgvtSQWROr2c14oiOGNQ2eJo4qSsn-s%3Dw360-h270-k-no!7i2048!8i1536!4m5!3m4!1s0x92a6bba82d1009c9:0xf8b205fbe791e347!8m2!3d-0.7628496!4d-48.511852',
           lat: '-0.7528755',
