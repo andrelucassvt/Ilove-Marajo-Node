5fb0f05069ec443475874b0ca5179b5e53ea912d
Kevin Sarges <kevinsarges08@gmail.com>
2021-05-06 10:04:04 -0300

    alteração

--- src/routes/routesMunicipios.js
+++ src/routes/routesMunicipios.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const municipio = require('../Controller/controllerMunicipios');
+const municipio = require('../controller/controllerMunicipios');
 
 router.get('/municipios', municipio.listMunicipios);
 router.get('/municipio', municipio.ListandoUmMunicipio);
